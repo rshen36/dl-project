5ceabb7ae4259ce70ff971ff0fc03647211879ff
Rich Shen <richard.d.shen@gmail.com>
2017-05-10 15:05:07 -0400

    fixing tf GPU issues v3

--- es_distributed/tf_util.py
+++ es_distributed/tf_util.py
@@ -4,7 +4,7 @@ import tensorflow as tf   # pylint: ignore-module ?
 import copy
 import os
 
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.40)
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.99)
 
 # ================================================================
 # Import all names into common namespace
